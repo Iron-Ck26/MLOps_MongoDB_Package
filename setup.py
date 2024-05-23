1ac009386cbda9e38bb9cd3a2f640d8117a0c363
Iron_Ck26 <chakrapaniwaghmode81@gmail.com>
2024-05-24 03:17:18 +0530

    updated code3

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import find_packages,setup
+"""from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT='-e .'
 
-"""def get_requirements(file_path:str)->List[str]:
+'''def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
@@ -12,7 +12,7 @@ HYPEN_E_DOT='-e .'
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
-    return requirements"""
+    return requirements'''
     
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
@@ -23,7 +23,7 @@ PKG_Name = "databaseautomation"  # "MongoDB-Connect"
 AUTHOR_USER_Name = "Iron-Ck"
 AUTHOR_EMAIL = "chakrapaniwaghmode8@gmail.com"
 
-"""
+'''
 setup(
     name='DimondPricePrediction',
     version='0.0.4',
@@ -32,7 +32,7 @@ setup(
     install_requires=["scikit-learn","pandas","numpy"],
     packages=find_packages()
 )
-"""
+'''
 setup(
     name=PKG_Name,
     version= __version__,
@@ -48,4 +48,53 @@ setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     #install_requires=get_requirements(".\requirements_dev.txt")
+)"""
+
+
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+__version__ = "0.0.4"
+REPO_NAME = "MLOps_MongoDB_Package"
+PKG_NAME = "databaseautomation"
+AUTHOR_USER_NAME = "Iron-Ck"
+AUTHOR_EMAIL = "chakrapaniwaghmode8@gmail.com"
+
+setup(
+    name=PKG_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for connecting with database",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=get_requirements("requirements.txt"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
