93291ca7f811f218d4ff8e96f4e2f36b40d045db
Iron_Ck26 <chakrapaniwaghmode81@gmail.com>
2024-05-24 02:45:41 +0530

    updated code2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from typing import List
 
 HYPEN_E_DOT='-e .'
 
-def get_requirements(file_path:str)->List[str]:
+"""def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
@@ -12,7 +12,7 @@ def get_requirements(file_path:str)->List[str]:
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
-    return requirements
+    return requirements"""
     
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
@@ -47,5 +47,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=get_requirements(".\requirements_dev.txt")
+    #install_requires=get_requirements(".\requirements_dev.txt")
 )
